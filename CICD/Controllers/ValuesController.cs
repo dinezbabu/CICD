296f7de30a1ccc3a294d9f5d9bbf8fef6cacf872
Dinesh Madesappa <NX81@dotcom.tesco.org>
2017-02-23 19:06:47 +0530

    Made it Working

--- CICD/Controllers/ValuesController.cs
+++ CICD/Controllers/ValuesController.cs
@@ -18,7 +18,7 @@ namespace CICD.Controllers
         // GET api/values/5
         public string Get(int id)
         {
-            return 1;
+            return "1";
         }
 
         // POST api/values
