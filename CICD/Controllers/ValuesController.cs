a7a773b8d9f748998c6ed24053e16eb5b7705286
Dinesh Madesappa <NX81@dotcom.tesco.org>
2017-02-28 19:27:02 +0530

    123

--- CICD/Controllers/ValuesController.cs
+++ CICD/Controllers/ValuesController.cs
@@ -12,7 +12,7 @@ namespace CICD.Controllers
         // GET api/values
         public IEnumerable<string> Get()
         {
-            return new string[] { "value12", "value2" };
+            return new string[] { "value1", "value2" };
         }
 
         // GET api/values/5
